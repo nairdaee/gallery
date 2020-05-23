ca7ac31fd86503fd7956db10a790e2c4ab52bba4
Adrian Etenyi <etadriano2@gmail.com>
2020-05-23 11:42:08 +0300

    add url for posting a photo

--- photos/urls.py
+++ photos/urls.py
@@ -6,5 +6,6 @@ app_name = 'photos'
 
 urlpatterns = [
     url(r'^$',views.photo_list,name="list"),
+    url(r"^create/$",views.photo_create,name="create"),
     url(r'^(?P<slug>[\w-]+)/$',views.photo_detail, name="detail"),
 ]
