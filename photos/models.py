d86ac96c38684333bcb3694e3f1fe87cd590b5d1
Adrian Etenyi <etadriano2@gmail.com>
2020-05-23 10:32:14 +0300

    add photos model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photos(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png',blank=True)
+    owner = models.ForeignKey(User, default= None)
+
+    def __str__(self):
+      return self.title
+
+    def snippet(self):
+        return self.body[:30] + '...'
