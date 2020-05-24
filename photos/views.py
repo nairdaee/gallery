6180f2354d4b78a35992c765511830cb1db3a4c2
Adrian Etenyi <etadriano2@gmail.com>
2020-05-24 13:11:49 +0300

    new media file,migrations and minor debugging

--- photos/views.py
+++ photos/views.py
@@ -1,16 +1,8 @@
-from django.shortcuts import render,redirect
-from .models import Photos
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Photos,Location, Category
 from django.http import HttpResponse
 from . import forms
 from django.contrib.auth.decorators import login_required
-
-def photo_list(request):
-    photos = Photos.objects.all().order_by('date')
-    return render(request,'photos/photos_list.html', {'photos': photos})
-
-def photo_detail(request,slug):
-   photos = Photos.objects.get(slug=slug)
-   return render(request,"photos/photos_detail.html",{'photo':photo})
 
 @login_required (login_url="/accounts/login/") #protects the view till user logs in
 def photo_create(request):
@@ -25,3 +17,34 @@ def photo_create(request):
     else:
         form = forms.CreatePhoto()
     return render(request,"photos/photos_create.html",{'form':form})
+
+
+def search_photos(request):
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        search_photos = Photos.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request,'photos/search.html',{"message":message,"photos_searched":search_photos})
+
+    else:
+        message = "You haven't searched yet"
+        return render(request,"photos/search.html",{"message":message})
+
+
+def filter_location(request,photo_id):
+    try:
+        location = Location.get_location()
+        located_photos = Photos.objects.filter(location=photo_id)
+    except DoesNotExist:
+        raise Http404()
+        return render(request,'photos/photos_detail.html',{"located_images":located_images,"locations":location})
+
+def photo_list(request):
+    photos = Photos.objects.all().order_by('date')
+    return render(request,'photos/photos_list.html', {'photos': photos})
+
+def photo_detail(request,slug):
+        photo = Category.objects.filter(slug=slug)   
+              ##photo = get_object_or_404(Photos, slug=slug)
+        return render(request,"photos/photos_detail.html",{'photo':photo})
