63f6ff0feeb11575f6775d4917445fbb32160f04
Adrian Etenyi <etadriano2@gmail.com>
2020-05-26 10:17:46 +0300

    configure static files

--- gallery/settings.py
+++ gallery/settings.py
@@ -128,11 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static-root')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
-
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles' 'media-root')
